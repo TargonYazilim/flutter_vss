8ba30813b9bef4fa9aa7c217166cd8724b5ef256
safauludogan <suludogan95@gmail.com>
2024-08-18 16:25:58 +0300

    /#45

--- lib/feature/splash/view_model/splash_view_model.dart
+++ lib/feature/splash/view_model/splash_view_model.dart
@@ -2,106 +2,18 @@
 
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_vss/feature/splash/view_model/state/splash_state.dart';
-import 'package:flutter_vss/product/cache/hive/hive_cache_operation.dart';
-import 'package:flutter_vss/product/service/interface/barcode_operation.dart';
-import 'package:flutter_vss/product/service/interface/order_operation.dart';
-import 'package:flutter_vss/product/service/model/barcode/barcode.dart';
-import 'package:flutter_vss/product/service/model/order/order.dart';
-import 'package:flutter_vss/product/service/model/order/order_detail.dart';
+import 'package:flutter_vss/product/service/manager/order/synchronize_cache_operation.dart';
 
 final class SplashViewModel extends Cubit<SplashState> {
-  SplashViewModel({
-    required OrderOperation orderOperation,
-    required BarcodeOperation barcodeOperation,
-    required HiveCacheOperation<Barcode> barcodeCacheOperation,
-    required HiveCacheOperation<Order> orderCacheOperation,
-  })  : _barcodeOperation = barcodeOperation,
-        _barcodeCacheOperation = barcodeCacheOperation,
-        _orderCacheOperation = orderCacheOperation,
-        _orderOperation = orderOperation,
+  SplashViewModel(
+      {required ISynchronizeCacheOperation synchronizeCacheOperation})
+      : _synchronizeCacheOperation = synchronizeCacheOperation,
         super(const SplashState(isLoading: false));
 
-  /// Order service
-  final OrderOperation _orderOperation;
+  ISynchronizeCacheOperation _synchronizeCacheOperation;
 
-  /// Hive cache operation for orders
-  final HiveCacheOperation<Order> _orderCacheOperation;
-
-  /// Barcode Service
-  final BarcodeOperation _barcodeOperation;
-
-  /// Hive barcode cache operation
-  final HiveCacheOperation<Barcode> _barcodeCacheOperation;
-
-  Future<void> getBarcodesFromDb() async {
-    final result = await _barcodeOperation.barcodes();
-    _saveBarcodes(result?.model?.items);
-  }
-
-  /// Save barcodes to hive cache
-  void _saveBarcodes(List<Barcode>? barcodes) {
-    if (barcodes == null) return;
-    _barcodeCacheOperation.addAll(barcodes);
-  }
-
-  /// Get orders from API
-  Future<void> getOrdersFromDb(String? userId) async {
-    if (userId != null) {
-      bool areDatasSynchronized = true;
-      // Sunucudan siparişleri al
-      final result = await _orderOperation.getOrder(int.parse(userId));
-
-      // Cache'den siparişleri al
-      final ordersFromCache = _orderCacheOperation.getAll();
-
-      final orders = result?.model?.orders ?? [];
-
-      if (orders.isNotEmpty) {
-        for (final order in orders) {
-          if (order.orderDetails != null && order.orderDetails!.isNotEmpty) {
-            // Sunucudan gelen siparişle cache'deki siparişi eşleştir
-            final cachedOrder = ordersFromCache.firstWhere(
-              (o) => o.siparisLogicalRef == order.siparisLogicalRef,
-              orElse: Order.empty,
-            );
-
-            if (cachedOrder.orderDetails != null) {
-              for (final orderDetail in order.orderDetails!) {
-                // Cache'deki sipariş detayını bul
-                final cachedDetail = cachedOrder.orderDetails!.firstWhere(
-                  (d) => d.siparisId == orderDetail.siparisId,
-                  orElse: OrderDetail.empty,
-                );
-
-                // Eğer cache'de scan varsa ve sunucudan gelen boşsa, sunucudan geleni temizle
-                if (cachedDetail.scans != null &&
-                    cachedDetail.scans!.isNotEmpty &&
-                    (orderDetail.scans == null || orderDetail.scans!.isEmpty)) {
-                  orderDetail.scans = cachedDetail.scans;
-                  areDatasSynchronized = false;
-                }
-                // Eğer sunucudan gelen scan varsa ve cache'de yoksa, sunucudan geleni temizle
-                else if (cachedDetail.scans == null ||
-                    cachedDetail.scans!.isEmpty) {
-                  orderDetail.scans?.clear();
-                  areDatasSynchronized = false;
-                }
-              }
-            }
-          }
-        }
-      }
-      if (!areDatasSynchronized) {
-        await _orderOperation.saveScanOrders(orders);
-      }
-      //TODO: Eğer if'in içerisine girdiyse. Veriyi local'e kayıt etmeden önce mevcut veriyi sunucuya gönder
-      _saveOrdersToCache(orders);
-    }
-  }
-
-  /// Save orders to hive cache
-  void _saveOrdersToCache(List<Order>? orders) {
-    if (orders == null) return;
-    _orderCacheOperation.addAll(orders);
+  Future<void> synchronizeDatas() async {
+    await _synchronizeCacheOperation.synchronizeOrders();
+    await _synchronizeCacheOperation.synchronizeBarcodes();
   }
 }
--- lib/product/state/container/product_state_container.dart
+++ lib/product/state/container/product_state_container.dart
@@ -1,6 +1,8 @@
 import 'package:flutter_vss/product/cache/hive/hive_cache_manager.dart';
 import 'package:flutter_vss/product/cache/product_cache.dart';
 import 'package:flutter_vss/product/cache/shared/shared_cache_operation.dart';
+import 'package:flutter_vss/product/service/manager/order/synchronize_cache_manager.dart';
+import 'package:flutter_vss/product/service/manager/order/synchronize_cache_operation.dart';
 import 'package:flutter_vss/product/service/manager/product_service_manager.dart';
 import 'package:flutter_vss/product/state/view_model/product_view_model.dart';
 import 'package:flutter_vss/product/utility/toast/toast_manager.dart';
@@ -15,13 +17,19 @@ final class ProductContainer {
 
   /// Product core required items
   static void setup() {
-          _getIt
-     ..registerSingleton<SharedCacheOperation>(SharedCacheOperation())
+    _getIt
+      ..registerSingleton<SharedCacheOperation>(SharedCacheOperation())
       ..registerLazySingleton<ToastManager>(ToastManager.new)
       ..registerLazySingleton<ToastService>(ToastManager.new)
       ..registerSingleton(ProductCache(cacheManager: HiveCacheManager()))
       ..registerSingleton<ProductNetworkManager>(ProductNetworkManager.base())
-      ..registerLazySingleton<ProductViewModel>(ProductViewModel.new);
+      ..registerLazySingleton<ProductViewModel>(ProductViewModel.new)
+      ..registerLazySingleton<ISynchronizeCacheOperation>(
+        SynchronizeCacheManager.new,
+      )
+      ..registerLazySingleton<SynchronizeCacheManager>(
+        SynchronizeCacheManager.new,
+      );
   }
 
   /// Read your dependency item for [ProductContainer]
